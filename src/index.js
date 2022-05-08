557679c17f9b193e9d9c17a2c03d8a5b0cf52571
Keith Estanislao <kestanislao09@outlook.com>
2022-05-08 14:55:46 -0700

    Asset management fixes

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 // import _ from 'lodash';
 import myName from './myName';
-import './style.css';
-import Doggo from './raincoat.png'
-// Uses CSV and XML loaders
-import Data from './data.xml'
-import Notes from './data.csv'
-// Uses custom parsers
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './datajson5';
+// import './style.css';
+// import Doggo from './raincoat.png'
+// // Uses CSV and XML loaders
+// import Data from './data.xml'
+// import Notes from './data.csv'
+// // Uses custom parsers
+// import toml from './data.toml';
+// import yaml from './data.yaml';
+// import json from './datajson5';
 
-// Logs data using custom parsers
-console.log(toml.title);
-console.log(toml.owner.name);
+// // Logs data using custom parsers
+// console.log(toml.title);
+// console.log(toml.owner.name);
 
-console.log(yaml.title);
-console.log(yaml.owner.crew);
+// console.log(yaml.title);
+// console.log(yaml.owner.crew);
 
-console.log(json.title);
-console.log(json.owner.bio);
+// console.log(json.title);
+// console.log(json.owner.bio);
 
 function component() {
   const element = document.createElement('div');
 
   // Using the myName function
   element.textContent = myName('Zorojuro');
-  element.classList.add('hello');
+  // element.classList.add('hello');
 
-  // Add an image that we imported above
-  const aPicture = new Image();
-  aPicture.src = Doggo;
-  element.appendChild(aPicture);
+  // // Add an image that we imported above
+  // const aPicture = new Image();
+  // aPicture.src = Doggo;
+  // element.appendChild(aPicture);
 
-  // Use imported data in things
-  console.log(Data);
-  console.log(Notes);
+  // // Use imported data in things
+  // console.log(Data);
+  // console.log(Notes);
 
   // // Lodash, imported up top by this script
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
-// Can use custom parsers
-// npm install --save-dev <PARSER> then configure:
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+// // Can use custom parsers
+// // npm install --save-dev <PARSER> then configure:
+// const toml = require('toml');
+// const yaml = require('yamljs');
+// const json5 = require('json5');
 
 module.exports = {
   entry: './src/index.js',
@@ -11,56 +11,57 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      // CSS Loader
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+  // //Asset Management
+  // module: {
+  //   rules: [
+  //     // CSS Loader
+  //     {
+  //       test: /\.css$/i,
+  //       use: ['style-loader', 'css-loader'],
+  //     },
 
-      // ## Asset Management
-      // Images
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      // Fonts
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      // CSV and XML loaders
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      // Custon Parsers
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
-  },
+  //     // ## Asset Management
+  //     // Images
+  //     {
+  //       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  //       type: 'asset/resource',
+  //     },
+  //     // Fonts
+  //     {
+  //       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+  //       type: 'asset/resource',
+  //     },
+  //     // CSV and XML loaders
+  //     {
+  //       test: /\.(csv|tsv)$/i,
+  //       use: ['csv-loader'],
+  //     },
+  //     {
+  //       test: /\.xml$/i,
+  //       use: ['xml-loader'],
+  //     },
+  //     // Custon Parsers
+  //     {
+  //       test: /\.toml$/i,
+  //       type: 'json',
+  //       parser: {
+  //         parse: toml.parse,
+  //       },
+  //     },
+  //     {
+  //       test: /\.yaml$/i,
+  //       type: 'json',
+  //       parser: {
+  //         parse: yaml.parse,
+  //       },
+  //     },
+  //     {
+  //       test: /\.json5$/i,
+  //       type: 'json',
+  //       parser: {
+  //         parse: json5.parse,
+  //       },
+  //     },
+  //   ],
+  // },
 };

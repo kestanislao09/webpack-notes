8a315670966e9504729f24c2a7615ebc208088a5
Keith Estanislao <kestanislao09@outlook.com>
2022-05-08 01:10:09 -0700

    css management

--- myName.js
+++
@@ -1,4 +0,0 @@
-const myName = (name) => 'Hi! my name is ' + name;
-
-
-export default myName;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Using the myName function
   element.textContent = myName('Zorojuro');
+  element.classList.add('hello');
 
   // // Lodash, imported up top by this script
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
